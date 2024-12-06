30f49a82148fb1ea686882028f5399d9d7a43332
guntur2006 <dektung123@gmail.com>
2024-12-06 10:52:18 +0800

    Add files via upload

---
+++ #membuat konversi dari m ke cm.py
@@ -0,0 +1,14 @@
+#membuat konversi dari m ke cm
+print("Tugas 2 algoritma mengkonversi satuan meter ke centimeter")
+
+#data diri
+print("nama: I Gusti Bagus Guntur Adhi Ganesha")
+print("nim : 2401010509")
+#judul awal
+print("masukan jumlah nilai yang ingin anda konvesi")
+#pengguna di harapkan memasukan nilai
+
+m=float (input("masukan nilai dalam meter:"))
+#merubah m ke dalam cm dengan menaikan 2 tinggkat berarti di x100
+cm=m*100
+print(f"{m}meter sama dengan {cm}centimeter.")
